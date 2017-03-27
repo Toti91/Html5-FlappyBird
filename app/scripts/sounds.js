0329f31db790f8e8902add273bf9926e3b7fde5f
Toti91 <torarinng@gmail.com>
2017-03-27 01:11:26 +0000

    fixed mute support for new sounds

--- app/scripts/sounds.js
+++ app/scripts/sounds.js
@@ -5,25 +5,38 @@ window.Sounds = (function() {
      * @constructor
      */
     var Sounds = function() {
-        this.s_song = new Audio('../sounds/Song.wav');
-        this.s_jump = new Audio('../sounds/JumpSound.wav');
-        this.s_crash = new Audio('../sounds/Crash.wav');
+        this.sounds = [];
 
+        this.s_song = new Audio('../sounds/Song.wav');
+        this.sounds.push(this.s_song);
+        this.s_jump = new Audio('../sounds/JumpSound.wav');
+        this.sounds.push(this.s_jump);
         this.b_annoying = new Audio('../sounds/dukNukem/annoying.wav');
+        this.sounds.push(this.b_annoying);
         this.g_bitchin = new Audio('../sounds/dukNukem/bitchin.wav');
+        this.sounds.push(this.g_bitchin);
         this.b_gameOver = new Audio('../sounds/dukNukem/game_over.wav');
+        this.sounds.push(this.b_gameOver);
         this.g_comeGetSome = new Audio('../sounds/dukNukem/come_get_some_x.wav');
+        this.sounds.push(this.g_comeGetSome);
         this.b_damn = new Audio('../sounds/dukNukem/damn.wav');
+        this.sounds.push(this.b_damn);
         this.b_damnIt = new Audio('../sounds/dukNukem/damn_it.wav');
+        this.sounds.push(this.b_damnIt);
         this.g_good = new Audio('../sounds/dukNukem/good2.wav');
+        this.sounds.push(this.g_good);
         this.b_gottaHurt = new Audio('../sounds/dukNukem/gotta_hurt.wav');
+        this.sounds.push(this.b_gottaHurt);
         this.g_hail = new Audio('../sounds/dukNukem/hail.wav');
+        this.sounds.push(this.g_hail);
         this.name = new Audio('../sounds/dukNukem/name.wav');
+        this.sounds.push(this.name);
         this.b_pissess = new Audio('../sounds/dukNukem/pisses_me.wav');
+        this.sounds.push(this.b_pissess);
         this.b_wasted = new Audio('../sounds/dukNukem/wasted.wav');
+        this.sounds.push(this.b_wasted);
 
         this.s_jump.volume = 0.2;
-        this.s_crash.volume = 0.2;
 
         this.mutebtn = document.getElementsByClassName('Mute')[0];
     };
@@ -45,25 +58,21 @@ window.Sounds = (function() {
         this.s_jump.play();
     };
 
-    Sounds.prototype.crashSound = function() {
-        this.s_crash.play();
-    }
-
     Sounds.prototype.muteAll = function() {
         if(!this.s_song.muted){
-            this.s_song.muted = true;
-            this.s_jump.muted = true;
-            this.s_crash.muted = true;
+            for(var i = 0; i < this.sounds.length; i++){
+                this.sounds[i].muted = true;
+            }
             this.mutebtn.innerHTML = "Unmute";
         }
         else{
-            this.s_song.muted = false;
-            this.s_jump.muted = false;
-            this.s_crash.muted = false;
+            for(var i = 0; i < this.sounds.length; i++){
+                this.sounds[i].muted = false;
+            }
             this.mutebtn.innerHTML = "Mute";
         }
     }
-    Sounds.prototype.goodSounds = function() {//chrasing sounds
+    Sounds.prototype.goodSounds = function() {// Completed sounds
        var number = Math.floor(Math.random() * 5) + 1;
        var song;
        console.log(number);
@@ -90,7 +99,7 @@ window.Sounds = (function() {
        song.play();
     } 
 
-    Sounds.prototype.damnCrashSound = function() { //Completed sounds
+    Sounds.prototype.damnCrashSound = function() { // Crash sounds
        var number = Math.floor(Math.random() * 6) + 1;
        var song;
        console.log(number);
